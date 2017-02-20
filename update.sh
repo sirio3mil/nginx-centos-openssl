ff86f7b3f6c96b97b3f90e1cc8518b58a9b677a3
Reynier de la Rosa <sirio3mil@users.noreply.github.com>
2017-02-20 10:16:36 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -8,3 +8,4 @@ sudo rpm -ivh http://nginx.org/packages/mainline/centos/$CENTVER/SRPMS/$NGINX.el
 sudo sed -i "s|--with-http_ssl_module|--with-http_ssl_module --with-openssl=/opt/lib/$OPENSSL|g" /root/rpmbuild/SPECS/nginx.spec
 sudo rpmbuild -ba /root/rpmbuild/SPECS/nginx.spec
 sudo rpm -Uvh --force /root/rpmbuild/RPMS/x86_64/$NGINX.el$CENTVER.centos.ngx.x86_64.rpm
+sudo rm /opt/lib/$OPENSSL.tar.gz
