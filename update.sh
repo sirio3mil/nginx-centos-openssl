d683f7006709481c8e49d0d35abdd0d460e81473
Reynier de la Rosa <sirio3mil@users.noreply.github.com>
2017-02-20 10:18:42 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ OPENSSL="openssl-1.1.0e"
 NGINX="nginx-1.11.10-1"
 sudo yum clean all
 sudo wget https://www.openssl.org/source/$OPENSSL.tar.gz -O /opt/lib/$OPENSSL.tar.gz
-sudo tar -zxvf /opt/lib/open* -C /opt/lib
+sudo tar -zxvf /opt/lib/$OPENSSL.tar.gz -C /opt/lib
 sudo rpm -ivh http://nginx.org/packages/mainline/centos/$CENTVER/SRPMS/$NGINX.el$CENTVER.ngx.src.rpm
 sudo sed -i "s|--with-http_ssl_module|--with-http_ssl_module --with-openssl=/opt/lib/$OPENSSL|g" /root/rpmbuild/SPECS/nginx.spec
 sudo rpmbuild -ba /root/rpmbuild/SPECS/nginx.spec
